f3c9594be8f9f742f0800fdf6fc83c8561fdb025
thallencar <thalencardev@gmail.com>
2024-08-23 17:46:04 -0300

    adicionando o package do automapper

--- src/HubArena.App/Data/ApplicationDbContext.cs
+++ src/HubArena.App/Data/ApplicationDbContext.cs
@@ -8,6 +8,6 @@ namespace HubArena.App.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
+        } 
     }
 }
