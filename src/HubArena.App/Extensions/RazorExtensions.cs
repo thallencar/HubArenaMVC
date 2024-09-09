ae0efaedd9ea6962545a185d6b45b0d0ac76c1c9
thallencar <thalencardev@gmail.com>
2024-09-09 18:16:52 -0300

    alterando views e criando razor extensions

---
+++ src/HubArena.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,39 @@
+﻿using HubArena.App.ViewModels;
+using HubArena.Business.Enums;
+using HubArena.Business.Interfaces;
+using HubArena.Business.Models;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace HubArena.App.Extensions
+{
+    public static class RazorExtensions
+    {
+
+        public static string StatusDoEquipamento(this RazorPage page, int statusEquipamento)
+        {
+            return statusEquipamento switch
+            {
+                (int)StatusEquipamentoEnum.Disponivel => "Disponível",
+                (int)StatusEquipamentoEnum.EmUso => "Em Uso",
+                (int)StatusEquipamentoEnum.Reservado => "Reservado",
+                (int)StatusEquipamentoEnum.Manutencao => "Em Manutenção",
+                (int)StatusEquipamentoEnum.Danificado => "Danificado",
+                (int)StatusEquipamentoEnum.Indisponivel => "Indisponível",
+            };
+        }
+        
+        public static string StatusDaQuadra(this RazorPage page, int statusQuadra)
+        {
+            return statusQuadra switch
+            {
+                (int)StatusQuadraEnum.Disponivel => "Disponível",
+                (int)StatusQuadraEnum.Reservada => "Reservada",
+                (int)StatusQuadraEnum.Ocupada => "Em Uso",
+                (int)StatusQuadraEnum.Manutencao => "Em Manutenção",
+                (int)StatusEquipamentoEnum.Indisponivel => "Indisponível",
+            };
+        } 
+
+    }
+}
+
--- src/HubArena.Business/Enums/TipoQuadraEnum.cs
+++ src/HubArena.Business/Enums/TipoQuadraEnum.cs
@@ -1,18 +1,18 @@
 ﻿namespace HubArena.Business.Enums
 {
-    [Flags]
+ 
     public enum TipoQuadraEnum
     {
-        None = 0,
-        Aberta = 1,
-        Fechada = 1<<1,
-        Poliesportiva = 1<<2,
-        Sintetica = 1<<3,
-        GramaNatural = 1<<4,
-        Areia = 1<<5,
-        Madeira = 1<<6,
-        Acrilica = 1<<7,
-        Concreto = 1<<8,
+       
+        Aberta,
+        Fechada,
+        Poliesportiva,
+        Sintetica,
+        GramaNatural,
+        Areia,
+        Madeira,
+        Acrilica,
+        Concreto,
 
         //0,1,2,4,8,16,32,64,128,256
         
